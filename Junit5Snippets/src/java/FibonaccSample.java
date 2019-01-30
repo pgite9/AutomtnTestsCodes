0da2057a57c71be51f5988fe0cf4b33c23b6f036
pgite9 <gite.paresh@gmail.com>
2019-01-30 10:52:33 -0500

    First Commit to Juni5

--- Junit5Snippets/src/java/FibonaccSample.java
+++ Junit5Snippets/src/java/FibonaccSample.java
@@ -50,4 +50,13 @@ public class FibonaccSample {
 
         }
     }
+
+    public static int factorial(int number){
+        //base case
+        if(number == 0){
+            return 1;
+        }
+        return number*factorial(number -1); //is this tail-recursion?
+    }
+
 }
